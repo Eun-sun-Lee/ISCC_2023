1d1fcb1c80d9bbfcd39271b7c844195911656194
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-01-19 16:17:45 +0900

    feat : 대회 때 사용 파일 업로드

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/foscar/ISCC_2023/devel/lib:/opt/ros/noetic/lib:/usr/local/cuda-11.4/lib64:/usr/local/cuda-11.4/lib64:/home/foscar/test_ws/devel/lib:/usr/local/cuda-11.4/lib64'
+export PWD='/home/foscar/ISCC_2023/build'
---
+++ build/vision_msgs/CMakeFiles/vision_msgs_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/BoundingBox2D.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/BoundingBox2DArray.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/BoundingBox3D.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/BoundingBox3DArray.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/Classification2D.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/Classification3D.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/Detection2D.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/Detection2DArray.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/Detection3D.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/Detection3DArray.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/ObjectHypothesis.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/ObjectHypothesisWithPose.lisp"
+  "/home/foscar/ISCC_2023/devel/share/common-lisp/ros/vision_msgs/msg/VisionInfo.lisp"
+  "CMakeFiles/vision_msgs_generate_messages_lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/vision_msgs_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
